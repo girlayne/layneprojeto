69f56b74af3d2bee587cce06ae1d12a6f572ff90
Kevin Sousa <kevinsmoura@hotmail.com>
2018-08-31 15:41:27 -0300

    Comitando repositorio compartilhado

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-echo "girlayne";
-?>
+<html>
+<head>
+	<title> Teste de Compartilhamento de Repositório </title>
+</head>
+<body>
+	<h1> Este arquivo foi feito na disciplina de Projeto e Prática </h1>
+	<?php echo "Oi Mundo !!! "; ?>
+</body>
+</html>
